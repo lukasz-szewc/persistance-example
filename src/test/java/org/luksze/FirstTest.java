44ce392f1af0a6c5902a3e06dd3287be504505e9
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2015-06-26 23:20:09 +0200

    added simple time counting for entity manager factory creation

--- src/test/java/org/luksze/FirstTest.java
+++ src/test/java/org/luksze/FirstTest.java
@@ -6,11 +6,20 @@ import org.junit.Test;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.time.Duration;
+import java.time.LocalTime;
 
 public class FirstTest {
     @Test
     public void testName() throws Exception {
+
+        LocalTime startTest = LocalTime.now();
+
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hsqldb-test-pu");
+
+        Duration from = Duration.between(startTest, LocalTime.now());
+        System.out.println("Seconds" + from.getSeconds() + " nanoseconds :" + from.toMillis() );
+
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
         entityManager.getTransaction().begin();
