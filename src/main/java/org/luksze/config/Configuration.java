4a3e9529c1300ebb2d5ab32f3e83f30db83a77f6
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2015-07-01 21:51:41 +0200

    introduced property file for connection configuration

--- src/main/java/org/luksze/config/Configuration.java
+++ src/main/java/org/luksze/config/Configuration.java
@@ -1,14 +1,34 @@
 package org.luksze.config;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
+import java.util.Properties;
 
-public class Configuration extends HashMap<Object, String> {
+public class Configuration extends HashMap<Object, Object> {
+
     public Configuration() {
-        put("javax.persistence.jdbc.url", "jdbc:h2:mem:cascade-test");
-        put("javax.persistence.jdbc.driver", "org.h2.Driver");
-        put("javax.persistence.jdbc.user", "sa");
-        put("javax.persistence.jdbc.password", "");
-        put("hibernate.hbm2ddl.auto", "create-drop");
-        put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        Properties properties = createProperties(readConfigurationFile());
+        for (String key : properties.stringPropertyNames()) {
+            put(key, properties.getProperty(key));
+        }
+    }
+
+    private InputStream readConfigurationFile() {
+        return Configuration.class.getClassLoader().getResourceAsStream("connection.properties");
+    }
+
+    private Properties createProperties(InputStream resourceAsStream) {
+        try {
+            return propertiesFrom(resourceAsStream);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private Properties propertiesFrom(InputStream resourceAsStream) throws IOException {
+        Properties properties = new Properties();
+        properties.load(resourceAsStream);
+        return properties;
     }
 }
