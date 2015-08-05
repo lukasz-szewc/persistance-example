25623baae2feb18d810f1baf0d61239e465a9cd6
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2015-08-05 16:00:40 +0200

    added simple possible test with JPA criteria

---
+++ src/test/java/org/luksze/criteria/SimpleCriteriaTest.java
@@ -0,0 +1,41 @@
+package org.luksze.criteria;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.luksze.CleanDatabaseTest;
+import org.luksze.Person;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class SimpleCriteriaTest extends CleanDatabaseTest {
+
+    public SimpleCriteriaTest() {
+        super("test-pu");
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        persistWithinTransaction(entityManager(), new Person("John", "Smith"));
+        persistWithinTransaction(entityManager(), new Person("Ann", "Snow"));
+        persistWithinTransaction(entityManager(), new Person("Emilia", "Sand"));
+    }
+
+    @Test
+    public void test() throws Exception {
+        EntityManager entityManager = entityManager();
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Person> criteriaQuery = criteriaBuilder.createQuery(Person.class);
+        Root<Person> root = criteriaQuery.from(Person.class);
+        criteriaQuery.select(root);
+        TypedQuery<Person> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Person> resultList = typedQuery.getResultList();
+        System.out.println(resultList);
+
+    }
+
+}
